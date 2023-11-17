778c9d23db44ee3d888de53fbf1ea0360253bfca
SnezhG <stim.nuzhno@mail.ru>
2023-11-17 22:58:19 +0300

    #Added Bootstrap

--- autoclient/src/App.jsx
+++ autoclient/src/App.jsx
@@ -1,19 +1,31 @@
 import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import FetchData from './FetchData'
 import Navbar from "@/Navbar.jsx";
 import "./styles.css"
 import Home from "./pages/Home"
-import Buses from "./pages/Buses"
-import Personnels from "./pages/Personnels"
-import Account from "./pages/Account"
+import Buses from "./pages/Buses/Buses.jsx"
 import CreateBus from "./pages/Buses/CreateBus"
 import EditBus from "./pages/Buses/EditBus"
-import IssueTripTicket from "./pages/IssueTripTicket"
-import Login from "./pages/Auth/Login"
-import TicketPay from "./pages/TicketPay"
-import Users from "./pages/Users.jsx"
+import Busroutes from "./pages/Busroutes/Busroutes.jsx"
+import CreateBusroute from "./pages/Busroutes/CreateBusroute"
+import EditBusroute from "./pages/Busroutes/EditBusroute"
+import Personnels from "./pages/Personnels/Personnels.jsx"
+import CreatePersonnel from "./pages/Personnels/CreatePersonnel"
+import EditPersonnel from "./pages/Personnels/EditPersonnel"
+import Trips from "./pages/Trip/Trips"
+import CreateTrip from "./pages/Trip/CreateTrip"
+import EditTrip from "./pages/Trip/EditTrip"
+import IssueTripTicket from "./pages/Ticket/IssueTripTicket"
+import TicketPay from "./pages/Ticket/TicketPay"
+import Ticket from "./pages/Ticket/Ticket.jsx"
+import ClientTickets from "./pages/Ticket/ClientTickets"
+import Users from "./pages/Users/Users.jsx"
 import CreateUser from "./pages/Users/CreateUser"
 import ChangeUserRole from "./pages/Users/ChangeUserRole"
+import Login from "./pages/Auth/Login"
+import Registration from "./pages/Auth/Registration"
+import Account from "./pages/Account"
 import { Route, Routes } from "react-router-dom"
 
 function App() {
@@ -24,16 +36,25 @@ function App() {
             <Routes>
                 <Route path="/" element={<Home />}/>
                 <Route path="/Buses" element={<Buses />}/>
-                <Route path="/Users" element={<Users />}/>
-                <Route path="/Personnels" element={<Personnels />}/>
-                <Route path="/Account" element={<Account />}/>
                 <Route path="/CreateBus" element={<CreateBus />}/>
+                <Route path="/EditBus/:id" element={<EditBus />}/>
+                <Route path="/Personnels" element={<Personnels />}/>
+                <Route path="/CreatePersonnel" element={<CreatePersonnel />}/>
+                <Route path="/EditPersonnel/:id" element={<EditPersonnel />}/>
+                <Route path="/Busroutes" element={<Busroutes />}/>
+                <Route path="/CreateBusroute" element={<CreateBusroute />}/>
+                <Route path="/EditBusroute/:id" element={<EditBusroute />}/>
+                <Route path="/Trips" element={<Trips />}/>
+                <Route path="/CreateTrip" element={<CreateTrip />}/>
+                <Route path="/EditTrip/:id" element={<EditTrip />}/>
+                <Route path="/Users" element={<Users />}/>
+                <Route path="/ChangeUserRole/:id" element={<ChangeUserRole />}/>
                 <Route path="/CreateUser" element={<CreateUser />}/>
                 <Route path="/Auth/Login" element={<Login />}/>
-                <Route path="/EditBus/:id" element={<EditBus />}/>
+                <Route path="/Auth/Registration" element={<Registration />}/>
                 <Route path="/TicketPay/:id" element={<TicketPay />}/>
                 <Route path="/IssueTripTicket/:id" element={<IssueTripTicket />}/>
-                <Route path="/ChangeUserRole/:id" element={<ChangeUserRole />}/>
+                <Route path="/Account" element={<Account />}/>
             </Routes>
         </div>
         </>
