1f1e35362c7ea0f041477b70623709d60366a047
SnezhG <stim.nuzhno@mail.ru>
2023-11-17 22:58:00 +0300

    #Navbar css

--- autoclient/src/Navbar.jsx
+++ autoclient/src/Navbar.jsx
@@ -1,21 +1,23 @@
 ﻿import { Link, useMatch, useResolvedPath } from "react-router-dom"
 export default function Navbar(){
     return (
-        <nav className="nav">
-        <Link to="/" className="site-title">
-            Bus Tickets
-        </Link>
-        <ul>
-            <li>
-                <CustomLink to="/Buses">Buses</CustomLink>
-            </li>
-            <li>
-                <CustomLink to="/Personnels">Personnels</CustomLink>
-            </li>
-            <li>
-                <CustomLink to="/Account">Account</CustomLink>
-            </li>
-        </ul>
+        <nav className="navbar navbar-expand navbar-dark bg-dark">
+            <div className="container">
+                <Link to="/" className="navbar-brand">
+                    Five Tickets at Freddy
+                </Link>
+                <ul className="navbar-nav">
+                    <li className="nav-item">
+                        <Link to="/Buses" className="nav-link">Автобусы</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to="/Personnels" className="nav-link">Персонал</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to="/Account" className="nav-link">Аккаунт</Link>
+                    </li>
+                </ul>
+            </div>
     </nav>
 )
 }
