d7c1f7f8cacf8136446253035203a4b7e3cb9248
SnezhG <stim.nuzhno@mail.ru>
2023-11-19 18:58:10 +0300

    #Added Trip and Ticket info

---
+++ AutoService/DTO/TicketInfoDTO.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Storage;
+
+namespace AutoService.DTO;
+
+public class TicketInfoDTO
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public string Patronymic { get; set; }
+    public string DepCity { get; set; }
+    public string ArrCity { get; set; }
+    public DateOnly DepDate { get; set; }
+    public string DepTime { get; set; }
+    public DateOnly ArrDate { get; set; }
+    public string ArrTime { get; set; }
+    public sbyte Seat { get; set; }
+    public decimal Price { get; set; }
+    public string Status { get; set; }
+}
---
+++ AutoService/DTO/TripInfoDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace AutoService.DTO;
+
+public class TripInfoDTO
+{
+    public int TripId { get; set; }
+    public string DepCity { get; set; }
+    public string ArrCity { get; set; }
+    public string DepTime { get; set; }
+    public DateOnly DepDate { get; set; }
+    public string ArrTime { get; set; }
+    public DateOnly ArrDate { get; set; }
+    public decimal Price { get; set; }
+}
