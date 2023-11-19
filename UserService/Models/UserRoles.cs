6acb1efe3b6af1a577c5ad50c251052965d1371e
SnezhG <stim.nuzhno@mail.ru>
2023-11-19 18:11:42 +0300

    #Deleted

--- UserService/Models/UserRoles.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace UserService.Models
-{
-    public static class UserRoles
-    {
-        public const string Client = "client";
-        public const string Admin = "admin";
-        public const string Dispatcher = "dispatcher";
-    }
-}
